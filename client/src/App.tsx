04452eb9d3b1edcd54cc5aaa39a3facf8175b7c8
David Marom <davidmarom.pro@gmail.com>
2022-01-28 21:34:29 +0200

    scss progress

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,24 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./assets/style/global.scss";
+
+import {
+  LinkProps,
+  Routes,
+  Route,
+  Outlet,
+  Link,
+  useMatch,
+  useResolvedPath,
+} from "react-router-dom";
+
+import { Home } from "./components/Home";
+import { About } from "./components/About";
+
+function CustomLink({ children, to, ...props }: LinkProps) {
+  let resolved = useResolvedPath(to);
+  let match = useMatch({ path: resolved.pathname, end: true });
+
+  return (
+    <div>
+      <Link
+        style={{ textDecoration: match ? "underline" : "none" }}
+        to={to}
+        {...props}
+      >
+        {children}
+      </Link>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="overall-layout">
+      <div className="side-bar">
+        <div>
+          <CustomLink to="/">Home</CustomLink>
+        </div>
+        <div>
+          <CustomLink to="/about">About</CustomLink>
+        </div>
+      </div>
+
+      <div className="right-side">
+        <div className="top-bar">Top-bar</div>
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="about" element={<About />} />
+          </Routes>
+        </div>
+
+      </div>
     </div>
   );
 }
